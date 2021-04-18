aceff1cc257a3ad56db4fe9451f7fe1107464074
Sheikh Norsam Saturgo <ssaturgo@gmail.com>
2021-04-17 19:20:40 -0700

    edited

--- Cogs/player_setup.py
+++ Cogs/player_setup.py
@@ -9,14 +9,27 @@ def profile_maker(name):
     with open(f"Database/Players/{name}.json", "r") as file :
         profile_json = json.load(file)
     with open(f"Database/Players/{name}.json", "w") as file :
-        profile_json["name"] = name
+        profile_json["Profile"]["Name"] = name
         json.dump(profile_json, file,indent=2)
 
-def view_profile(path):
+def view_profile(path, name, pfp):
+    with open(f"Database/emoji.json", "r") as file :
+        emojis = json.load(file) 
     player_description = ""
     with open(f"{path}", "r") as file :
         profile_json = json.load(file)
-    player_description += profile_json["Profile"]
+    player_description += (
+        f"{emojis['Profile']['Name']} | {profile_json['Profile']['Name']}\n" +
+        f"{emojis['Profile']['Money']} | {profile_json['Profile']['Wealth']}\n"
+        f"{emojis['Profile']['Collections']} | {profile_json['Profile']['Tamed']} / 20\n"
+    )
+    embed_msg = discord.Embed(
+        title=name,
+        description=player_description,
+        color=0xfef3c0,
+        thumbnail=pfp
+    )
+    return embed_msg
 
 class Player_Setup(commands.Cog):
     def __init__(self, client):
@@ -35,8 +48,12 @@ class Player_Setup(commands.Cog):
             shutil.copyfile("Database/profile_template.json", f"{file_path}")
             profile_maker(username)
             print(f"Created new directory for {username}")
+            profile = view_profile(file_path, username, ctx.author.member.avatar_url)
+            await ctx.send(embed=profile)
         else :
             print(f"{username} is an existing player.. loading profile")
+            profile = view_profile(file_path, username, ctx.author.member.avatar_url)
+            await ctx.send(embed=profile)
 
 def setup(client):
     client.add_cog(Player_Setup(client))
--- test.py
+++ test.py
@@ -3,5 +3,7 @@ import json
 def view_profile(path):
     with open(f"{path}", "r") as file :
         profile_json = json.load(file)
+    for i in profile_json["Profile"]:
+        print (i)
 
 view_profile("Database/profile_template.json")
